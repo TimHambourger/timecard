3b9b568cbe23533e8e04ece9f19f6abe7946d7ea
Tim Hambourger <tim.hambourger@gmail.com>
2015-07-12 22:21:54 -0500

    Rounder -- change rounding 'all' algorithm to be NlogN instead of N^2

--- rounder.js
+++ rounder.js
@@ -20,6 +20,11 @@ Rounder.prototype.roundTasks = function (tasks) {
 
     if (this.rounding === Strategies.each) return roundedTasks;
 
+    if (this.rounding !== Strategies.all) throw new Error('Unrecognized rounding value: \'' + this.rounding + '\'.');
+
+    // The difficult case. Adjust the rounded tasks so that the total of the adjusted times is the same as rounding the true total.
+
+    // First compare the rounded true total and the total of the rounded times and assess which direction we need to adjust in.
     var trueTotal = 0;
     for (var task in tasks)
         trueTotal += tasks[task];
@@ -33,24 +38,30 @@ Rounder.prototype.roundTasks = function (tasks) {
     var roundingError = roundedTotal - roundedTasksTotal;
     if (roundingError === 0) return roundedTasks;
 
+    // Now sort tasks by those whose true time is farthest from their rounded time.
+    // We'll adjust these tasks first.
+    var tasksByRoundingError = [];
+    for (var task in tasks) {
+        tasksByRoundingError.push(task);
+    }
+
+    var multiplier = roundingError > 0 ? 1 : -1;
+
+    // Perform the sort, respecting the multiplier and putting the tasks that most need adjusting first.
+    tasksByRoundingError.sort(function (taskA, taskB) {
+        return multiplier * (tasks[taskB] - roundedTasks[taskB] + roundedTasks[taskA] - tasks[taskA]);
+    });
+
     var adjustedTasks = {};
     for (var task in tasks) {
         adjustedTasks[task] = roundedTasks[task];
     }
 
-    while (roundingError !== 0) {
-        var multiplier = roundingError > 0 ? 1 : -1,
-            biggestError = 0,
-            biggestErrorTask = null;
+    var i = 0; 
 
-        for (var task in tasks) {
-            var error = tasks[task] - adjustedTasks[task];
-            if (error * multiplier > biggestError * multiplier) {
-                biggestError = error;
-                biggestErrorTask = task;
-            }
-        }
-        adjustedTasks[biggestErrorTask] += this.roundTo * multiplier;
+    // Keep adjusting till we eliminate the rounding error.
+    while (roundingError !== 0) {
+        adjustedTasks[tasksByRoundingError[i++]] += this.roundTo * multiplier;
         roundingError -= this.roundTo * multiplier;
     }
 
